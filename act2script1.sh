68338c5c6d5a79f8de6b252255d6f37153b4f5a6
root <root@rubioa.vejd1ocdraoupbxykrn13eem5e.bx.internal.cloudapp.net>
2024-03-27 18:32:14 -0400

    act2script1.sh

---
+++ act2script1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "Tell me your username: " userName
+
+echo "Hello, $userName. Welcome to your Virtual Machine!"
---
+++ act2script3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+time=$(date +"%H")
+
+read -p "Name: " name
+sleep 1
+
+if [ $time -lt 12 ]
+then
+	echo "Good morning, $name!"
+elif [ $time -ge  12 ] && [ $time -lt 17 ]
+then 
+	echo "Good afternoon, $name!"
+else
+	echo "Good evening, $name!"
+fi
+
